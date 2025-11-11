ac81ca854afb11580d40bb65bc3cac24c52b08d0
goss2701 <h098s02701@chunchon.gwe.hs.kr>
2025-11-11 11:19:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     labels[0]: {
        "texts": ["레제는 폭탄의 악마입니다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://www.youtube.com/shorts/vUW_Xm0W2q0"]
+      "videos": ["https://www.youtube.com/shorts/vUW_Xm0W2q0"]
      },
 }
 
